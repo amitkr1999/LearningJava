0a418ee9c76bbcc3bb2ca98f942013fab22df260
Amit <amitkr21999@gmail.com>
2022-11-03 23:10:33 +0530

    class_and_object

---
+++ src/com/company/intermediate/classesandobject/add.java
@@ -0,0 +1,23 @@
+package com.company.intermediate.classesandobject;
+
+import java.util.Scanner;
+
+public class add {
+   int a, b;
+   void getdata() {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter first number");
+       a = sc.nextInt();
+       System.out.println("Enter second number");
+       b = sc.nextInt();
+   }
+   void putdata(){
+       System.out.println("sum of two number is " + (a+b));
+   }
+    public static void main(String[] args) {
+       add aa = new add();
+       aa.getdata();
+       aa.putdata();
+
+    }
+}
