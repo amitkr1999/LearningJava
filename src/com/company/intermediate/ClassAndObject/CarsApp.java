6aa3fa4320aebc7e1632a55c09a6510ddd71d83e
Amit <amitkr21999@gmail.com>
2022-11-04 13:02:18 +0530

    class_and_object

---
+++ src/com/company/intermediate/ClassAndObject/CarsApp.java
@@ -0,0 +1,10 @@
+package com.company.intermediate.ClassAndObject;
+
+public class CarsApp {
+    public static void main(String[] args) {
+        DodgeChallenger dodgeChallenger = new DodgeChallenger();
+        dodgeChallenger.getDescription();
+        dodgeChallenger.startTheEngine();
+
+    }
+}
