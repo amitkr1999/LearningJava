b83ad1fdb2751acafef63cc48a33ad588cabafc4
Amit <amitkr21999@gmail.com>
2022-11-10 18:31:08 +0530

    Abstraction

---
+++ src/com/company/intermediate/Abstraction/ElectricMode.java
@@ -0,0 +1,7 @@
+package com.company.intermediate.Abstraction;
+
+public interface ElectricMode {
+    void startTheElectricEngine();
+
+
+}
---
+++ src/com/company/intermediate/Abstraction/ToyotaSupra.java
@@ -0,0 +1,14 @@
+package com.company.intermediate.Abstraction;
+
+import com.company.intermediate.Encapsulation.Car;
+
+public class ToyotaSupra extends Car {
+    public ToyotaSupra(String color, byte numberOfVehicleOwners, short power, short horsePower, short cubicCapacity, int price, int mileage, boolean isDamaged) {
+        super("Toyota Supra", color, numberOfVehicleOwners, power, horsePower, cubicCapacity, price, mileage, isDamaged);
+    }
+
+    @Override
+    public void startTheEngine() {
+        System.out.println("A Toyota Supra Engine is started.");
+    }
+}
