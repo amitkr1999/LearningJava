e6b937f23e1d34913116a5df2131066b16d2dd1c
Amit <amitkr21999@gmail.com>
2022-11-13 16:51:00 +0530

    Final Keyword

---
+++ src/com/company/intermediate/FinalKeyword/Brazil.java
@@ -0,0 +1,8 @@
+package com.company.intermediate.FinalKeyword;
+
+public class Brazil extends Country {
+    public Brazil(String name, int population, String capital) {
+        super(name, population, capital);
+    }
+
+}
---
+++ src/com/company/intermediate/FinalKeyword/Country.java
@@ -0,0 +1,18 @@
+package com.company.intermediate.FinalKeyword;
+
+public class Country {
+
+    private final String NAME;
+    private int population;
+    private String capital;
+
+    public Country(String name, int population, String capital) {
+        NAME = name;
+        this.population = population;
+        this.capital = capital;
+    }
+
+    public final void populationChange(int change){
+        population += change;
+    }
+}
