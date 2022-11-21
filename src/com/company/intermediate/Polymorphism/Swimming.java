ea6fedd3d23464971ddc44b1dd59ffa17095beb6
Amit <amitkr21999@gmail.com>
2022-11-21 23:04:29 +0530

    Polymorphism

--- src/com/company/intermediate/Polymorphism/Swimming.java
+++ src/com/company/intermediate/Polymorphism/Swimming.java
@@ -1,6 +1,7 @@
 package com.company.intermediate.Polymorphism;
 //interface is just like a class in java, it contains abstract methods. A class implements interface and inheriting the abstract methods of the interface.
 public interface Swimming {
+    //abstract method
     void freestyle();
     void freestyle(int distance);
 }
