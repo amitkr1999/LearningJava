f7aaf5e936840e7f43b7022bec5b4c58a62658f3
Amit <amitkr21999@gmail.com>
2023-05-31 17:20:19 +0530

    Interface

--- src/com/company/intermediate/Polymorphism/Swimming.java
+++ src/com/company/intermediate/Polymorphism/Swimming.java
@@ -6,8 +6,8 @@ public interface Swimming {
 
     void freestyle(int distance);
 }
-//interface is just like a class in java, it contains abstract methods.
-// for example below I have defined interface as Swimmer with two abstract method
-//FreeStyle() and freeStyle( with parameter distance)
+// Interface is just like a class in java, it contains abstract methods.
+// For example below I have defined interface as Swimmer with two abstract method
+// FreeStyle() and freeStyle( with parameter distance)
 // A class implements interface and inheriting the abstract methods of the interface.
-//for example this interface is implemented in Swimmer.java class and above two methods are override and defined its body
+// For example this interface is implemented in Swimmer.java class and above two methods are override and defined its body
